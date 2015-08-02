6978398837fe8e4a27e38ae85fe44a8ff54d9bad
onthebeachh <s.cardenas@raboit.com>
2015-08-02 02:26:24 -0400

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://45.55.83.34/admin/');
+define('HTTP_CATALOG', 'http://45.55.83.34/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://45.55.83.34/admin/');
+define('HTTPS_CATALOG', 'http://45.55.83.34/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'secreto123');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://45.55.83.34/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://45.55.83.34/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'secreto123');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
