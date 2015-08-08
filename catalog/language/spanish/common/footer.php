2b1d130e1cc8a3ab182a7909aa83c7d50fafe64f
onthebeachh <s.cardenas@raboit.com>
2015-08-08 19:25:12 -0400

    theme upleaded

---
+++ catalog/language/spanish/common/footer.php
@@ -0,0 +1,19 @@
+<?php
+// Text
+$_['text_information']  = 'Información';
+$_['text_service']      = 'Servicio clientes';
+$_['text_extra']        = 'Extras';
+$_['text_account']      = 'Cuenta';
+$_['text_contact']      = 'Contáctanos';
+$_['text_return']       = 'Devoluciones';
+$_['text_sitemap']      = 'Mapa del sitio';
+$_['text_manufacturer'] = 'Marcas';
+$_['text_voucher']      = 'Vales regalo';
+$_['text_affiliate']    = 'Afiliados';
+$_['text_special']      = 'Especiales';
+$_['text_login']        = 'Inicio sesión';
+$_['text_order']        = 'Historial pedidos';
+$_['text_wishlist']     = 'Lista de deseos';
+$_['text_newsletter']   = 'Boletín de noticias';
+$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s // Traducido por ComputerGarden.com.ve';
+?>
---
+++ catalog/language/spanish/common/header.php
@@ -0,0 +1,14 @@
+<?php
+// Text
+$_['text_home']     = 'Inicio';
+$_['text_wishlist'] = 'Lista de deseos (%s)';
+$_['text_cart']     = 'Cesta';
+$_['text_items']    = '%s ítem(s) - %s';
+$_['text_search']   = 'Búsqueda';
+$_['text_welcome']  = 'Bienvenido, puedes <a href="%s">iniciar sesión</a> o <a href="%s">crear una cuenta</a>.';
+$_['text_logged']   = 'Has iniciado sesión como <a href="%s">%s</a> <b>(</b> <a href="%s">Salir</a> <b>)</b>';
+$_['text_account']  = 'Mi cuenta';
+$_['text_checkout'] = 'Realizar pedido';
+$_['text_language'] = 'Idioma';
+$_['text_currency'] = 'Moneda';
+?>
